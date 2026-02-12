6b0cda523db9fdef0509202e7a3efaf54d9e783b
Gerry&Tech <davidecorsaro2009@gmail.com>
2026-02-12 23:13:16 +0100

    apps.js

--- apps/apps.js
+++ apps/apps.js
@@ -5,7 +5,8 @@ const APPS = [
     desc: "Premi il pulsante, animazione LED suspense e il servo ruota in base al risultato.",
     tags: ["SERVO", "LED", "GAME"],
     requires: "UNO Q, LED, Servo, Pulsante",
-    zip: "https://github.com/gerry-tech/gerry-uno-q-apps/raw/refs/heads/main/apps/roulette-servo/Roulette.zip",
+    zip: "zip: "https://github.com/gerry-tech/gerry-uno-q-apps/raw/refs/heads/main/apps/roulette-servo/Roulette.zip",
+",
     demo: "",
     thumbText: "Roulette"
   }
